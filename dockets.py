1a44b6d4f11003a8eb0e9b3e3606bdfbd5784b5c
Selah Lynch <selahlynch@gmail.com>
2016-07-10 20:36:38 -0400

    first stab at a script

---
+++ dockets.py
@@ -0,0 +1,17 @@
+import requests
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfparser import PDFParser
+print "Reading some dockets"
+
+#url = "https://ujsportal.pacourts.us/DocketSheets/CPReport.ashx?docketNumber=CP-51-CR-0000862-2013"
+#page = requests.get(url)
+open_file = open("/home/selah/Code/dockets/docket_sample.pdf")
+# Create a PDF parser object associated with the file object.
+parser = PDFParser(open_file)
+# Create a PDF document object that stores the document structure.
+doc = PDFDocument(parser)
+# Connect the parser and document objects.
+print parser.nextline()
+
+
+
